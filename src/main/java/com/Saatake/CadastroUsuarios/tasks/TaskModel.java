64c6732f59a314ec653e5b75e54ea4817d8e13e4
Edson Satake <edsonsatake510@gmail.com>
2025-08-30 22:29:31 -0300

    Adição do Lombok para trabalhar com Constutores e Getters e Setters

--- src/main/java/com/Saatake/CadastroUsuarios/tasks/TaskModel.java
+++ src/main/java/com/Saatake/CadastroUsuarios/tasks/TaskModel.java
@@ -2,14 +2,19 @@ package com.Saatake.CadastroUsuarios.tasks;
 
 import com.Saatake.CadastroUsuarios.user.UserModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.ArrayList
-import class.UserModel
-
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "tb_tarefas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TaskModel {
 
     @Id
@@ -17,51 +22,13 @@ public class TaskModel {
     private Long id;
     private String nome;
     private String descricao;
+
+    @Enumerated(EnumType.STRING)
     private TaskStatus status;
+
     private LocalDateTime dataCriacao;
 
     @OneToMany(mappedBy = "tarefas")
     private List<UserModel> user = new ArrayList<UserModel>();
 
-    public TaskModel() {
-    }
-
-    public TaskModel(String nome, String descricao, TaskStatus status, LocalDateTime dataCriacao) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.status = status;
-        this.dataCriacao = dataCriacao;
-    }=
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public TaskStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getDataCriacao() {
-        return dataCriacao;
-    }
-
-    public void setDataCriacao(LocalDateTime dataCriacao) {
-        this.dataCriacao = dataCriacao;
-    }
 }
--- src/main/java/com/Saatake/CadastroUsuarios/user/UserModel.java
+++ src/main/java/com/Saatake/CadastroUsuarios/user/UserModel.java
@@ -2,12 +2,17 @@ package com.Saatake.CadastroUsuarios.user;
 
 import com.Saatake.CadastroUsuarios.tasks.TaskModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-import package.TaskModel
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UserModel {
 
     @Id
@@ -22,36 +27,5 @@ public class UserModel {
     private  TaskModel tarefas;
 
 
-    public UserModel() {
-    }
 
-    public UserModel(String nome, String email, Integer idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
 }
